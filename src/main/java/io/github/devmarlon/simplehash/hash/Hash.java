bdb9ac1aa6fcfac3364dad1a5b0697b4891cd432
marlon <mo14421890@gmail.com>
2026-05-25 01:30:52 -0300

    method for converting a hash to a string

--- src/main/java/io/github/devmarlon/simplehash/hash/Hash.java
+++ src/main/java/io/github/devmarlon/simplehash/hash/Hash.java
@@ -1,5 +1,7 @@
 package io.github.devmarlon.simplehash.hash;
 
+import java.util.List;
+
 public class Hash {
 
     private final String hash;
@@ -8,7 +10,11 @@ public class Hash {
         this.hash = hash;
     }
 
-    public static Hash from(HashBuilder hash) {
-        return new Hash(hash.getHash());
+    public static Hash from(HashBuilder hashBuilder) {
+        return new Hash(hashBuilder.getHash());
+    }
+
+    public String HashtoString() {
+        return this.hash;
     }
 }
--- src/main/java/io/github/devmarlon/simplehash/hash/HashBuilder.java
+++ src/main/java/io/github/devmarlon/simplehash/hash/HashBuilder.java
@@ -6,18 +6,24 @@ public class HashBuilder {
 
     private String[] rawParams;
     private String buildedHash;
-    private final int defaultSize = 5;
+    private boolean hashHasBuilded;
 
 
     public HashBuilder() {
     }
 
     public static HashBuilder of() {
-        return new HashBuilder();
+        return new HashBuilder().configure((S) -> S.updateStatus(false) );
     }
 
-    public static HashBuilder create(String[] rawParams) {
-        return HashBuilder.of().configure( (S) -> S.addParams(rawParams) );
+    public Hash toHash() {
+        this.gen();
+        return Hash.from( this );
+    }
+
+    private HashBuilder updateStatus(boolean s) {
+        this.hashHasBuilded = s;
+        return this;
     }
 
     public HashBuilder configure(UnaryOperator<HashBuilder> configurator) {
@@ -25,28 +31,35 @@ public class HashBuilder {
     }
 
     public HashBuilder addParams(String... buildHash) {
-        if (buildHash.length !=  defaultSize) {
-            throw new IllegalArgumentException();
-        }
+        hashHasBuilded = false;
         if (this.buildedHash == null) {
             this.rawParams = buildHash;
         }
         return this;
     }
 
+    private void gen() {
+        this.buildedHash = this.generateHash(rawParams);
+    }
+
     private String generateHash(String[] par) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < par.length - 1; i++) {
-            if (i != par.length - 1) {
+        int arraySize = par.length;
+        for (int i = 0; i < arraySize; i++) {
+            if (i != arraySize - 1) {
                 sb.append(par[i].toUpperCase()).append("-");
             }else {
                 sb.append(par[i].toUpperCase());
             }
+            updateStatus(true);
         }
         return sb.toString();
     }
 
     public String getHash() {
+        if (!hashHasBuilded) {
+            throw new IllegalArgumentException("Hash não está criado");
+        }
         return this.buildedHash;
     }
 }
